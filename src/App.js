bc3bd838ca553c2b4e9397f120fce1cf8ce27994
Severino Vinicius <severino.sales95@gmail.com>
2023-04-19 16:53:07 -0300

    Requisito 2 em teste

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        {/* render={ () => <Home research={ research } /> } */}
+      </Switch>
     </div>
   );
 }
