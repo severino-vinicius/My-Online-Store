a29238558a8488a21017de3611b6a6377b32920b
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-04-20 03:15:02 -0300

    [Grupo 23] Adições para início do 7

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
+// import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route path="/shopping-cart" component={ ShoppingCart } />
+        {/* <Route path="/product-detail/:id" component={ ProductDetail } /> */}
       </Switch>
     </div>
   );
