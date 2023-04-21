449755ec2bb6c429a14145b7e00c7bbf38d75c95
Juan Cassius Carneiro Pereira <cassjuan@hotmail.com>
2023-04-20 23:59:09 -0300

    [Grupo 23] Requisito 11 terminado. Falta criar o componente Evaluation

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -6,9 +6,20 @@ import ShoppingCartButton from './ShoppingCartButton';
 class ProductDetails extends Component {
   state = {
     productDetails: [],
+    evaluations: [],
+    errorMsg: false,
+    evaluation: {
+      email: '',
+      text: '',
+      rating: '',
+    },
   };
 
   componentDidMount() {
+    const { evaluations } = this.state;
+    if (evaluations.length === 0) {
+      this.restoreEvaluations();
+    }
     this.hadleId();
   }
 
@@ -45,11 +56,66 @@ class ProductDetails extends Component {
     }
   };
 
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState((prevState) => ({
+      errorMsg: false, // Sempre que algo for modificado significa que o erro não existe mais, caso contrário errorMsg seria true, então sempre que algo for modificado, o erro é resetado
+      evaluation: {
+        ...prevState.evaluation,
+        [name]: value,
+      },
+    }));
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { match: { params: { id } } } = this.props;
+    const { evaluation: { email, rating } } = this.state;
+    if (!(email.includes('@')) || !rating) { // Verifica se o email é válido e se o rating foi selecionado
+      this.setState({
+        errorMsg: true, // Se o email não for válido ou o rating não for selecionado, errorMsg é true
+      });
+    } else { // Senão continua com o processo de adicionar a avaliação
+      const { evaluation } = this.state;
+      if (!localStorage.getItem(`${id}`)) {
+        localStorage.setItem(`${id}`, JSON.stringify([])); // Se não existir o localStorage com o id do produto, cria um
+      }
+      const evaluations = JSON.parse(localStorage.getItem(`${id}`));
+      evaluations.push(evaluation);
+      localStorage.setItem(`${id}`, JSON.stringify(evaluations));
+      this.setState({ // Reseta o errorMsg caso ele ainda exista (Meio que impossível mas vai saber né)
+        errorMsg: false,
+        evaluations, // Adiciona a avaliação ao estado para a renderização ser feita
+      }, () => {
+        this.setState({ // Reseta o estado evaluation para que o formulário fique vazio
+          evaluation: {
+            email: '',
+            text: '',
+            rating: '',
+          },
+        });
+      });
+    }
+  };
+
+  restoreEvaluations = () => { // Função que restaura as avaliações do localStorage
+    const { match: { params: { id } } } = this.props;
+    if (!localStorage.getItem(`${id}`)) {
+      localStorage.setItem(`${id}`, JSON.stringify([]));
+    }
+    const evaluations = JSON.parse(localStorage.getItem(`${id}`));
+    this.setState({
+      evaluations,
+    });
+  };
+
   render() {
-    const { productDetails } = this.state;
+    const { productDetails, evaluations,
+      errorMsg, evaluation: { email, text } } = this.state;
     return (
       <div>
         <h1>ProductDetails</h1>
+        <ShoppingCartButton />
         <img
           src={ productDetails.thumbnail }
           alt="productDetails.title"
@@ -61,13 +127,87 @@ class ProductDetails extends Component {
         <p data-testid="product-detail-price">
           { productDetails.price }
         </p>
-        <button
-          onClick={ this.addToCart }
-          data-testid="product-detail-add-to-cart"
-        >
+        <button onClick={ this.addToCart } data-testid="product-detail-add-to-cart">
           Adicionar ao carrinho
         </button>
-        <ShoppingCartButton />
+        <h2>Avaliação do produo</h2>
+        <form>
+          <label>
+            Email:
+            <input
+              data-testid="product-detail-email"
+              onChange={ this.handleChange }
+              value={ email } // O value do input é o estado evaluation.email, senão o input não seria controlado
+              type="email"
+              name="email"
+            />
+          </label>
+          <label>
+            Avaliação:
+            <input
+              onChange={ this.handleChange }
+              data-testid="1-rating"
+              type="radio"
+              value="1"
+              name="rating"
+            />
+            <input
+              onChange={ this.handleChange }
+              data-testid="2-rating"
+              type="radio"
+              value="2"
+              name="rating"
+            />
+            <input
+              onChange={ this.handleChange }
+              data-testid="3-rating"
+              type="radio"
+              value="3"
+              name="rating"
+            />
+            <input
+              onChange={ this.handleChange }
+              data-testid="4-rating"
+              type="radio"
+              value="4"
+              name="rating"
+            />
+            <input
+              onChange={ this.handleChange }
+              data-testid="5-rating"
+              type="radio"
+              value="5"
+              name="rating"
+            />
+          </label>
+          rating
+          <label>
+            Comentário(Opcional):
+            <textarea
+              data-testid="product-detail-evaluation"
+              onChange={ this.handleChange }
+              value={ text } // O value do input é o estado evaluation.text, senão o input não seria controlado
+              type="text"
+              name="text"
+            />
+          </label>
+          <button
+            data-testid="submit-review-btn"
+            type="submit"
+            onClick={ this.handleSubmit }
+          >
+            Enviar
+          </button>
+          {errorMsg && <p data-testid="error-msg">Campos inválidos</p>}
+        </form>
+        {evaluations.length === 0
+          ? <p>Não há avaliações</p> : evaluations.map((evaluation, index) => (
+            <div key={ index }>
+              <p data-testid="review-card-email">{evaluation.email}</p>
+              <p data-testid="review-card-rating">{evaluation.rating}</p>
+              <p data-testid="review-card-evaluation">{evaluation.text}</p>
+            </div>
+          ))}
       </div>
     );
   }
